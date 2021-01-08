effc568cff98585154e8bfb8bf28625e06f43ee9
naoto hamada <hamada@vis-its.com>
2021-01-08 23:22:47 +0900

    Add eslint

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
 
-export default function Home() {
+export default () => {
   const router = useRouter();
   const { id } = router.query;
 
@@ -57,4 +57,4 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
