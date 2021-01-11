7d3d06e05e379a94c88ebd6bfdf77f0b64293b39
naoto hamada <hamada@vis-its.com>
2021-01-12 01:20:43 +0900

    Add styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import theme from '../theme';
+import Footer from '../components/Footer';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
+  useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles && jssStyles.parentElement) {
       jssStyles.parentElement.removeChild(jssStyles);
@@ -18,13 +20,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <React.Fragment>
       <Head>
         <title>keiba post</title>
-        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <StyledComponentsThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+            <Footer />
+          </StyledComponentsThemeProvider>
+        </ThemeProvider>
+      </StylesProvider>
     </React.Fragment>
   );
 }
