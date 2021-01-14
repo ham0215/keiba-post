c2313f1adb928896aba9ffcaa245e95ced201120
naoto hamada <hamada@vis-its.com>
2021-01-15 00:11:47 +0900

    Add post button

--- src/pages/keiba/[id]/post/index.tsx
+++ src/pages/keiba/[id]/post/index.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 import styled from 'styled-components'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -17,35 +18,46 @@ const ButtonArea = styled.div`
   text-align: center;
 `;
 
+type FormInputType = {
+  keibaText: string;
+};
+
 export default function Post() {
   const router = useRouter();
   const { id } = router.query;
+
+  const { register, handleSubmit } = useForm<FormInputType>();
 
   const onClickCancel = useCallback(() => {
     router.push(`/keiba/${id}`)
   }, [id, router]);
 
-  const onClickPost = useCallback(() => {
-    router.push(`/keiba/${id}`)
+  const onClickPost = useCallback((data) => {
+    router.push({ pathname: `/keiba/${id}`, query: { text: data.keibaText } });
   }, [id, router]);
 
   return (
     <>
-      <KeibaTextField
-        label="予想"
-        multiline
-        fullWidth
-        rows={10}
-        variant="outlined"
-      />
-      <ButtonArea>
-        < KeibaButton variant="outlined" onClick={onClickCancel}>
-          キャンセル
-        </KeibaButton>
-        <KeibaButton variant="outlined" color="primary" onClick={onClickPost}>
-          投稿
-        </KeibaButton>
-      </ButtonArea>
+      <form onSubmit={handleSubmit(onClickPost)}>
+        <KeibaTextField
+          id="keibaText"
+          name="keibaText"
+          label="予想"
+          multiline
+          fullWidth
+          rows={10}
+          variant="outlined"
+          inputProps={{ ref: register({ required: true }) }}
+        />
+        <ButtonArea>
+          <KeibaButton variant="outlined" onClick={onClickCancel}>
+            キャンセル
+          </KeibaButton>
+          <KeibaButton type="submit" variant="outlined" color="primary">
+            投稿
+          </KeibaButton>
+        </ButtonArea>
+      </form>
     </>
   );
 }
