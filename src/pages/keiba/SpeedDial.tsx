e6fec39ffcb9070327a9f6c75f03eeafb0b2b9da
naoto hamada <hamada@vis-its.com>
2021-01-14 23:15:52 +0900

    Fix speed dial

--- src/pages/keiba/SpeedDial.tsx
+++ src/pages/keiba/SpeedDial.tsx
@@ -3,19 +3,11 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import SpeedDial from '@material-ui/lab/SpeedDial';
 import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
 import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
-import SaveIcon from '@material-ui/icons/Save';
-import PrintIcon from '@material-ui/icons/Print';
-import ShareIcon from '@material-ui/icons/Share';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import ImageIcon from '@material-ui/icons/Image';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      height: 380,
-      transform: 'translateZ(0px)',
-      flexGrow: 1,
-    },
     speedDial: {
       position: 'absolute',
       bottom: theme.spacing(2),
@@ -25,11 +17,8 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
-  { icon: <FavoriteIcon />, name: 'Like' },
+  { icon: <ImageIcon />, name: 'Image' },
+  { icon: <TextFieldsIcon />, name: 'Text' },
 ];
 
 type Props = {
@@ -49,26 +38,24 @@ export default function SpeedDialTooltipOpen({ open, setOpen }: Props) {
   };
 
   return (
-    <div className={classes.root}>
-      <SpeedDial
-        ariaLabel="SpeedDial"
-        className={classes.speedDial}
-        hidden={false}
-        icon={<SpeedDialIcon />}
-        onClose={handleClose}
-        onOpen={handleOpen}
-        open={open}
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-            tooltipOpen
-            onClick={handleClose}
-          />
-        ))}
-      </SpeedDial>
-    </div>
+    <SpeedDial
+      ariaLabel="SpeedDial"
+      className={classes.speedDial}
+      hidden={false}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          tooltipOpen
+          onClick={handleClose}
+        />
+      ))}
+    </SpeedDial>
   );
 }
