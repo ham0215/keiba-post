2be16ea06fbd97a658ed4e8e809ddbff01f42a9d
naoto hamada <hamada@vis-its.com>
2021-02-07 13:13:33 +0900

    Add baseUrl, paths

--- src/firestore/Post.ts
+++ src/firestore/Post.ts
@@ -1,4 +1,4 @@
-import firebase from '../firebase';
+import firebase from 'firebase';
 
 export async function updataPost(keibaId: string, uid: string, text: string, createdAt: Date) {
   await firebase.firestore().collection('keibas').doc(keibaId).collection('posts').doc(uid).set({
