895d6b2edad5e3626430d7e34f0f6335946a1e6a
naoto hamada <hamada@vis-its.com>
2021-02-21 00:40:02 +0900

    Add Result (#23)

---
+++ src/pages/result/ResultCard.tsx
@@ -0,0 +1,30 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import styled from 'styled-components';
+import ExternalLink from 'components/ExternalLink'
+import { Icon } from 'semantic-ui-react'
+
+type Props = {
+  title: string;
+  winner?: string;
+  url: string;
+}
+
+const Span = styled.span`
+  margin-left: 10px;
+  margin-right: 5px;
+`;
+
+export default function ResultCard({ title, winner, url }: Props) {
+  return (
+    <Card>
+      <CardContent>
+        <ExternalLink href={url} variant="body1">{title}</ExternalLink>
+        <Span>
+          <Icon name='trophy' size='large' color='yellow' />
+        </Span>
+        {winner}
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,6 +1,11 @@
 import { useContext } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import Error from 'components/Error';
-import ExternalLink from 'components/ExternalLink'
+import ExternalLink from 'components/ExternalLink';
+import { Icon } from 'semantic-ui-react';
+import ResultCard from './ResultCard';
+
 import { UserContext } from 'UserContext';
 
 export default function Result() {
@@ -10,12 +15,9 @@ export default function Result() {
 
   return (
     <>
-      <div>
-        <ExternalLink href="https://public.flourish.studio/visualisation/1807679/" variant="body1">2020年前半戦</ExternalLink>
-      </div>
-      <div>
-        <ExternalLink href="https://public.flourish.studio/visualisation/3919343/" variant="body1">2020年後半戦</ExternalLink>
-      </div>
+      <ResultCard url="https://public.flourish.studio/visualisation/5349437/" title='2021年前半戦' winner='???' />
+      <ResultCard url="https://public.flourish.studio/visualisation/3919343/" title='2020年後半戦' winner='natsu' />
+      <ResultCard url="https://public.flourish.studio/visualisation/1807679/" title='2020年前半戦' winner='ham' />
     </>
   );
 }
