a9cd5ca6072daafd850473c5ab1ff41d221b8e69
naoto hamada <hamada@vis-its.com>
2021-11-16 01:47:37 +0900

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+  '@mui/icons-material'
+]);
+
+module.exports = withTM({
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+     '@mui/styled-engine': '@mui/styled-engine-sc',
+     };
+     return config;
+  },
   reactStrictMode: true
-}
+});
