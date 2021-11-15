ca7816b14419b6b9820ef72ec6090be1301e3e57
naoto hamada <hamada@vis-its.com>
2021-11-16 02:23:04 +0900

    Add emotion

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withTM({
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-     '@mui/styled-engine': '@mui/styled-engine-sc',
+      '@mui/styled-engine': '@mui/styled-engine-sc',
      };
      return config;
   },
