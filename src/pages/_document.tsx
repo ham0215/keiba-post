e45d149fe49461703c661c5c84bf0c1bbe55077c
naoto hamada <hamada@vis-its.com>
2021-11-16 00:17:03 +0900

    Refactoring

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import ServerStyleSheets from '@mui/styles/ServerStyleSheets';
 import { ServerStyleSheet } from 'styled-components';
 import theme from 'theme';
 
@@ -49,24 +48,26 @@ MyDocument.getInitialProps = async (ctx) => {
   // 4. page.render
 
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const styledComponentsSheet = new ServerStyleSheet();
+  const sheet = new ServerStyleSheet();
   const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => styledComponentsSheet.collectStyles(sheets.collect(<App {...props} />)),
-    });
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
 
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      styledComponentsSheet.getStyleElement(),
-      sheets.getStyleElement(),
-    ],
-  };
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <React.Fragment>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </React.Fragment>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
 };
