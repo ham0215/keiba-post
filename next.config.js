4a430a620e9fe890b9947178d25a202f4eab3448
naoto hamada <hamada@vis-its.com>
2021-11-16 09:23:16 +0900

    Refactoring

--- next.config.js
+++ next.config.js
@@ -5,11 +5,5 @@ const withTM = require('next-transpile-modules')([
 ]);
 
 module.exports = withTM({
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias
-    };
-    return config;
-  },
+  reactStrictMode: true
 });
