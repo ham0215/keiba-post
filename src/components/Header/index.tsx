e34c67be6cc71ef8b230ccadb5e93df175e418b0
naoto hamada <hamada@vis-its.com>
2021-12-29 01:14:16 +0900

    Fix header (#136)
    * Fix header
    
    * Fix prettier

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,9 @@
-import React, { useContext } from 'react';
 import styled from '@emotion/styled';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { useRouter } from 'next/router';
-import Menu from './Menu';
-import { UserContext } from 'UserContext';
-import UserAvatar from 'components/UserAvatar';
+import Link from 'components/Link';
+import UserMenu from './UserMenu';
 
 const Header = styled.header`
   flex-grow: 1;
@@ -20,31 +16,14 @@ const Title = styled(Typography)`
 `;
 
 export default function ButtonAppBar() {
-  const router = useRouter();
-
-  const { currentUser } = useContext(UserContext);
-
-  let loginButton;
-  if (currentUser) {
-    const { url } = currentUser;
-    loginButton = <UserAvatar url={url} />;
-  } else {
-    loginButton = (
-      <Button color="inherit" onClick={() => router.push('/login')}>
-        Login
-      </Button>
-    );
-  }
-
   return (
     <Header>
       <AppBar position="static" color="transparent">
         <Toolbar>
-          <Menu user={currentUser} />
           <Title variant="h6">
-            <span onClick={() => router.push('/')}>Keiba Post</span>
+            <Link href="/">Keiba Post</Link>
           </Title>
-          {loginButton}
+          <UserMenu />
         </Toolbar>
       </AppBar>
     </Header>
---
+++ src/components/Link/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import NextLink from 'next/link';
+import Link from '@mui/material/Link';
+
+type Props = { href: string; prefetch?: boolean; children: ReactNode };
+
+export default function LinkComponent({ href, prefetch = false, children }: Props) {
+  return (
+    <NextLink href={href} prefetch={prefetch}>
+      <Link href={href} color="inherit" underline="none">
+        {children}
+      </Link>
+    </NextLink>
+  );
+}
