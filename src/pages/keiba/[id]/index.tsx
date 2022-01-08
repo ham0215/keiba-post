5b0d3d54ab5860102bc1f4b46e947d662316e8f5
naoto hamada <hamada@vis-its.com>
2022-01-09 00:18:21 +0900

    Add posts

--- src/pages/keiba/[id]/index.tsx
+++ src/pages/keiba/[id]/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Grid from '@mui/material/Grid';
 import { getFirestore, getDocs, collection } from 'firebase/firestore';
