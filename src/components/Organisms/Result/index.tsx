c1a024854f992200c18a42f2cf30210478e854f9
naoto hamada <hamada@vis-its.com>
2022-03-02 23:27:35 +0900

    Update (#206)
    * Update
    
    * Refactoring
    
    * Refactoring
    
    * Add speeddialog
    
    * Add result
    
    * WIP
    
    * Update packages
    
    * Fix error
    
    * Add canpost
    
    * WIP
    
    * Fix prettier
    
    * Remove unused component

---
+++ src/components/Organisms/Result/index.tsx
@@ -0,0 +1,92 @@
+import { useState, useContext, useEffect, useCallback } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import styled from '@emotion/styled';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import { findBets } from 'firestore/Keiba';
+import TextField from 'components/TextField';
+import Button from 'components/Button';
+import Error from 'components/Error';
+import { UserContext } from 'UserContext';
+
+const ButtonArea = styled.div`
+  text-align: center;
+`;
+
+type FormInputType = {
+  keibaText: string;
+};
+
+export default function Result() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const {
+    handleSubmit,
+    formState: { isDirty, isSubmitted, isValid },
+  } = useForm<FormInputType>({
+    mode: 'all',
+  });
+
+  const { currentUser } = useContext(UserContext);
+
+  const [bets, setBets] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      if (!id) return;
+      if (typeof id !== 'string') return;
+      if (!currentUser) return;
+
+      setBets(await findBets(id));
+    })();
+  }, [currentUser, id]);
+
+  const onClickCancel = useCallback(() => {
+    router.push(`/keiba/${id}`);
+  }, [id, router]);
+
+  const onClickPost = useCallback(async () => {
+    if (!currentUser) return;
+
+    router.push({ pathname: `/keiba/${id}` });
+  }, [currentUser, id, router]);
+
+  if (!currentUser) return <Error />;
+
+  return (
+    <form onSubmit={handleSubmit(onClickPost)}>
+      <List sx={{ width: '100%', maxWidth: 360, margin: '0 auto', padding: 0 }}>
+        {bets.map((bet, index) => (
+          <>
+            <ListItem sx={{ padding: 0 }}>
+              <ListItemAvatar>
+                <Avatar alt={`result${index}`} src={bet} />
+              </ListItemAvatar>
+              <TextField
+                key={index}
+                id={`result${index}`}
+                name={`result${index}`}
+                variant="standard"
+                type="number"
+                fullWidth
+                defaultValue=""
+              />
+            </ListItem>
+          </>
+        ))}
+      </List>
+      <ButtonArea>
+        <Button variant="outlined" onClick={onClickCancel}>
+          キャンセル
+        </Button>
+        <Button type="submit" variant="contained" disabled={!isDirty || isSubmitted || !isValid}>
+          更新
+        </Button>
+      </ButtonArea>
+    </form>
+  );
+}
--- src/components/SpeedDial.tsx
+++ src/components/SpeedDial.tsx
@@ -3,15 +3,17 @@ import { useRouter } from 'next/router';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
-import TextFieldsIcon from '@mui/icons-material/TextFields';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import BedroomBabyIcon from '@mui/icons-material/BedroomBaby';
 
 type Props = {
   open: boolean;
   setOpen: (open: boolean) => void;
   keibaId: number;
+  canPost: boolean;
 };
 
-export default function SpeedDialTooltipOpen({ open, setOpen, keibaId }: Props) {
+export default function SpeedDialComponent({ open, setOpen, keibaId, canPost }: Props) {
   const router = useRouter();
 
   const handleOpen = useCallback(() => {
@@ -26,6 +28,10 @@ export default function SpeedDialTooltipOpen({ open, setOpen, keibaId }: Props) 
     router.push(`/keiba/${keibaId}/post`);
   }, [keibaId, router]);
 
+  const onClickResult = useCallback(() => {
+    router.push(`/keiba/${keibaId}/result`);
+  }, [keibaId, router]);
+
   return (
     <SpeedDial
       ariaLabel="SpeedDial"
@@ -36,7 +42,17 @@ export default function SpeedDialTooltipOpen({ open, setOpen, keibaId }: Props) 
       onOpen={handleOpen}
       open={open}
     >
-      <SpeedDialAction key="text" icon={<TextFieldsIcon />} tooltipTitle="text" tooltipOpen onClick={onClickText} />
+      {canPost ? (
+        <SpeedDialAction key="text" icon={<BedroomBabyIcon />} tooltipTitle="投票" tooltipOpen onClick={onClickText} />
+      ) : (
+        <SpeedDialAction
+          key="result"
+          icon={<EmojiEventsIcon />}
+          tooltipTitle="結果"
+          tooltipOpen
+          onClick={onClickResult}
+        />
+      )}
     </SpeedDial>
   );
 }
