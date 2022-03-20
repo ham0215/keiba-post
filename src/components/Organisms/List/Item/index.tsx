c5f8c88204b02306c02da99242cabadf50e92f5e
naoto hamada <hamada@vis-its.com>
2022-03-20 22:07:41 +0900

    Refactoring

--- src/components/Organisms/List/Item/index.tsx
+++ src/components/Organisms/List/Item/index.tsx
@@ -19,15 +19,18 @@ const AvatarGroupRow = styled(AvatarGroup)`
   flex-direction: row;
 `;
 
+
 export default function Item({ id, date, big, name, tag, bets }: Props) {
   const router = useRouter();
 
+  const onClick = () => router.push({ pathname: `/keiba/${id}`, query: { tag } });
+
   return (
-    <TableRow onClick={() => router.push({ pathname: `/keiba/${id}`, query: { tag } })}>
-      <TableCell component="th" scope="row">
+    <TableRow>
+      <TableCell component="th" scope="row" onClick={onClick}>
         {date.substring(5)}
       </TableCell>
-      <TableCell>
+      <TableCell onClick={onClick}>
         {big ? (
           <Badge variant="dot" color="error">
             {name}
@@ -36,7 +39,7 @@ export default function Item({ id, date, big, name, tag, bets }: Props) {
           name
         )}
       </TableCell>
-      <TableCell>
+      <TableCell onClick={onClick}>
         <AvatarGroupRow max={10}>
           {bets.map((bet, index) => (
             <Avatar key={index} src={bet} sx={{ width: 24, height: 24 }} />
