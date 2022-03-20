0a29f657ccb724d55a73c4150135c570ba02189b
naoto hamada <hamada@vis-its.com>
2022-03-21 00:52:56 +0900

    Fix pretter

--- src/pages/keiba/[id]/index.tsx
+++ src/pages/keiba/[id]/index.tsx
@@ -2,5 +2,9 @@ import WithAuth from 'components/templates/WithAuth';
 import Detail from 'components/Organisms/Detail';
 
 export default function DetailPage() {
-  return <WithAuth><Detail /></WithAuth>;
+  return (
+    <WithAuth>
+      <Detail />
+    </WithAuth>
+  );
 }
--- src/pages/keiba/[id]/result/index.tsx
+++ src/pages/keiba/[id]/result/index.tsx
@@ -2,5 +2,9 @@ import WithAuth from 'components/templates/WithAuth';
 import Result from 'components/Organisms/Result';
 
 export default function ResultPage() {
-  return <WithAuth><Result /></WithAuth>;
+  return (
+    <WithAuth>
+      <Result />
+    </WithAuth>
+  );
 }
