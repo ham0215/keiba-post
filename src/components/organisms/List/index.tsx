859714bc98a4a8d415cb9c366bda3c01a41e6d98
naoto hamada <hamada@vis-its.com>
2022-03-28 13:49:12 +0900

    Fix

---
+++ src/components/organisms/List/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from '@emotion/styled';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { findKeibas, Keiba } from 'firestore/Keiba';
+import Row from './Row';
+import Header from './Header';
+import NextRace from './NextRace';
+import { KeibaCalendar, currentTag, tags } from 'data/KeibaCalendar';
+
+const StyledHeader = styled(TableHead)`
+  background-color: lightgray;
+`;
+
+export default function List() {
+  const [keibas, setKeibas] = useState<Keiba[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      setKeibas(await findKeibas());
+    })();
+  }, []);
+
+  const router = useRouter();
+  const { tag } = router.query;
+  const selectedTag = typeof tag === 'string' && tags.includes(tag) ? tag : currentTag;
+
+  return (
+    <>
+      <Header tag={selectedTag} />
+      <NextRace tag={selectedTag} />
+      <TableContainer component={Paper}>
+        <Table aria-label="keiba calendar">
+          <StyledHeader>
+            <TableRow>
+              <TableCell>日付</TableCell>
+              <TableCell>レース名</TableCell>
+              <TableCell>的中！</TableCell>
+            </TableRow>
+          </StyledHeader>
+          <TableBody>
+            {KeibaCalendar.filter((row) => row.tag === selectedTag).map((row) => (
+              <Row
+                key={row.id}
+                {...row}
+                bets={keibas.find((keiba) => keiba.id === row.id)?.bets || []}
+                results={keibas.find((keiba) => keiba.id === row.id)?.results || []}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
---
+++ src/components/organisms/Top/index.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import KeibaButton from './KeibaButton';
+import Result from './Result';
+
+const Contents = styled.div`
+  background: #151515 url('/images/login.jpg') no-repeat scroll center bottom;
+  background-size: cover;
+  color: #fff;
+  padding: 5rem 0;
+  text-align: center;
+  @media (min-width: 767px) {
+    height: 87vh;
+  }
+`;
+
+export default function Top() {
+  return (
+    <Contents>
+      <KeibaButton />
+      <Result />
+    </Contents>
+  );
+}
