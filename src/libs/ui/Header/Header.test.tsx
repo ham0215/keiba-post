f1da18759bc8f98fbdcd9c8bdd12587f101abec2
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-01-01 02:02:20 +0900

    Fix test

--- src/libs/ui/Header/Header.test.tsx
+++ src/libs/ui/Header/Header.test.tsx
@@ -1,6 +1,8 @@
 import { Header } from './Header.container';
 import { render } from '@testing-library/react';
 
+jest.mock('next/router', () => require('next-router-mock'));
+
 describe('Header', () => {
   it('snapshot testing', () => {
     const { asFragment } = render(<Header />);
