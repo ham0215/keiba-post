4544cd88b35e238d0c10ad3179a3fdb12669630d
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-01-03 00:45:24 +0900

    Refactor post page

---
+++ src/libs/features/PostKeiba/components/Form.component.tsx
@@ -0,0 +1,39 @@
+import TextField from 'libs/ui/TextField';
+import Button from 'libs/ui/Button';
+import * as Styles from './Form.styles';
+import { useFormPresenter } from './Form.presenter';
+import type { FormInputType } from '../PostKeiba.models';
+
+type Props = {
+  onSubmit: (input: FormInputType) => void;
+  onCancel: () => void;
+  postText: string;
+};
+
+export function Form({ onSubmit, onCancel, postText }: Props) {
+  const { register, handleSubmit, disabledSubmit } = useFormPresenter();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        id="keibaText"
+        name="keibaText"
+        label="予想"
+        multiline
+        fullWidth
+        rows={10}
+        variant="outlined"
+        defaultValue={postText}
+        inputProps={{ ...register('keibaText', { required: true }) }}
+      />
+      <Styles.ButtonArea>
+        <Button variant="outlined" onClick={onCancel}>
+          キャンセル
+        </Button>
+        <Button type="submit" variant="contained" disabled={disabledSubmit}>
+          投稿
+        </Button>
+      </Styles.ButtonArea>
+    </form>
+  );
+}
--- src/pages/keiba/[id]/post/index.tsx
+++ src/pages/keiba/[id]/post/index.tsx
@@ -1,96 +1,11 @@
-import { useState, useCallback, useContext, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { useForm } from 'react-hook-form';
-import styled from '@emotion/styled';
-import { findPostText } from 'libs/firestore/Keiba';
-import TextField from 'libs/ui/TextField';
-import Button from 'libs/ui/Button';
-import { updatePost } from 'libs/firestore/Keiba';
+import { useContext } from 'react';
 import { UserContext } from 'libs/hooks/UserContext';
 import { PleaseLogin } from 'libs/features/PleaseLogin';
-
-const ButtonArea = styled.div`
-  text-align: center;
-`;
-
-type FormInputType = {
-  keibaText: string;
-};
+import { PostKeiba } from 'libs/features/PostKeiba';
 
 export default function Post() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  const {
-    register,
-    handleSubmit,
-    formState: { isDirty, isSubmitted, isValid }
-  } = useForm<FormInputType>({
-    mode: 'all'
-  });
-
   const { currentUser } = useContext(UserContext);
-
-  const [postText, setPostText] = useState<string>('');
-
-  useEffect(() => {
-    (async () => {
-      if (!id) return;
-      if (typeof id !== 'string') return;
-      if (!currentUser) return;
-
-      const text = await findPostText(id, currentUser.id);
-      if (!text) return;
-
-      setPostText(text);
-    })();
-  }, [currentUser, id]);
-
-  const onClickCancel = useCallback(() => {
-    router.push(`/keiba/${id}`);
-  }, [id, router]);
-
-  const onClickPost = useCallback(
-    async ({ keibaText }: { keibaText: string }) => {
-      if (!currentUser) return;
-      if (typeof id !== 'string') return;
-
-      await updatePost({
-        keibaId: id,
-        uid: currentUser.id,
-        name: currentUser.name,
-        url: currentUser.url,
-        text: keibaText,
-        createdAt: new Date()
-      });
-      router.push({ pathname: `/keiba/${id}` });
-    },
-    [currentUser, id, router]
-  );
-
   if (!currentUser || !currentUser.enabled) return <PleaseLogin />;
 
-  return (
-    <form onSubmit={handleSubmit(onClickPost)}>
-      <TextField
-        id="keibaText"
-        name="keibaText"
-        label="予想"
-        multiline
-        fullWidth
-        rows={10}
-        variant="outlined"
-        defaultValue={postText}
-        inputProps={{ ...register('keibaText', { required: true }) }}
-      />
-      <ButtonArea>
-        <Button variant="outlined" onClick={onClickCancel}>
-          キャンセル
-        </Button>
-        <Button type="submit" variant="contained" disabled={!isDirty || isSubmitted || !isValid}>
-          投稿
-        </Button>
-      </ButtonArea>
-    </form>
-  );
+  return <PostKeiba currentUser={currentUser} />;
 }
