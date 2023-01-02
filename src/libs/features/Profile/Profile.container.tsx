c04426283c38ffa634e2f6f0de5d858c5db71316
Naoto Hamada <naoto.hamada@findy.co.jp>
2023-01-02 23:19:42 +0900

    Refactor profile page

---
+++ src/libs/features/Profile/Profile.container.tsx
@@ -0,0 +1,17 @@
+import Card from 'libs/ui/Card';
+import { Form } from './components/Form';
+import { useProfileFacade } from './Profile.facade';
+
+export function Profile() {
+  const {
+    handleCancel,
+    handleSubmit,
+    currentUser
+  } = useProfileFacade();
+
+  return (
+    <Card>
+      <Form onCancel={handleCancel} onSubmit={handleSubmit} currentUser={currentUser} />
+    </Card>
+  );
+}
